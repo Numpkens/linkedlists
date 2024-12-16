40a66580b8b99a2bc87b7fd51c51acead2a6d0f1
Numpkens <numpkins1228@gmail.com>
2024-12-17 05:37:02 +0800

    Add methods

---
+++ linked.rb
@@ -0,0 +1,53 @@
+Class LinkedList
+
+def append
+  LinkedList.append(value)
+end
+
+def prepend
+  LinkedList.prepend(value)
+end
+
+def size
+  LinkedList.size
+end
+
+def head
+  LinkedList.head
+end
+
+def tail
+  LinkedList.tail
+end
+
+def at
+  LinkedList.at(index)
+end
+
+def pop
+  LinkedList.pop
+end
+
+def contains
+  LinkedList.contains?(value)
+end
+
+def find
+  LinkedList.find(value)
+end
+
+def to_s
+  LinkedList.to_s
+end
+
+def insert_at
+  LinkedList.insert_at(value, index)
+end
+
+def remove_at
+  LinkedList.remove_at(index)
+end
+
+Class Node
+
+nil
